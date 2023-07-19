551dda61cbb8cd5e89c5cc20f469e99fe32f93fd
samitkumarsinha <samitkumarsinha@gmail.com>
2023-07-19 12:36:30 +0530

    add

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,15 +1,32 @@
-import { Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { DynamicComponent } from '../dynamic/dynamic.component';
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent {
-  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef }) container!: ViewContainerRef;
-  componentRef!: ComponentRef<DynamicComponent>;
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef })
+  container!: ViewContainerRef;
+  dynaComponentRefs: ComponentRef<DynamicComponent>[] = [];
   createDynamicComponent() {
-    this.componentRef = this.container.createComponent(DynamicComponent);
-    this.componentRef.instance.message = 'Hello, Dynamic Component!';
+    const componentRef = this.container.createComponent(DynamicComponent);
+    componentRef.instance.message = 'Hello, Dynamic Component!';
+    componentRef.instance.closeEvent.subscribe(() =>
+      this.closeDynamicComponent(componentRef)
+    );
+    this.dynaComponentRefs.push(componentRef);
+  }
+  closeDynamicComponent(componentRef: ComponentRef<DynamicComponent>) {
+    const index = this.dynaComponentRefs.indexOf(componentRef);
+    if (index !== -1) {
+      componentRef.destroy();
+      this.dynaComponentRefs.splice(index, 1);
+    }
   }
 }
--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic',
@@ -7,4 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class DynamicComponent {
   @Input() message: string = '';
+  @Output() closeEvent = new EventEmitter<void>();
+
+  close() {
+    this.closeEvent.emit();
+  }
+
 }
